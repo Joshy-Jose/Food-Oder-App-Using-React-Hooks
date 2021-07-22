fe667e607b95433c2f50391aea00cdaade0532b3
joshyjose <joshyjose12@gmail.com>
2021-07-22 13:06:12 +0200

    Implement Animation to cart (Bump)

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,20 +1,43 @@
 import React from 'react';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
 
+    const [btnIsHighlated, setBtnIsHighlated] = useState(false);
+    
     const cartCtx = useContext(CartContext);   //header cart button component will be re-evaluvated 
                                                 //by react when ever the cart context changes,changes do updated in cart provider component
 
-    const numgerOfCartItems = cartCtx.item.reduce((curNumber, item) => {
+    const {item} = cartCtx;
+
+    const numgerOfCartItems = item.reduce((curNumber, item) => {
 
         return curNumber + item.amount;
     },0);   
+    const btnClasses = `${classes.button} ${btnIsHighlated ? classes.bump : ''}`;
 
-    return <button className={classes.button} onClick={props.onClick}>
+    useEffect(() => {
+        if(item.length === 0) {
+            return;
+        }
+        setBtnIsHighlated(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlated(false)
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        };
+
+    },[item])
+   
+   
+   
+   return <button className={btnClasses} onClick={props.onClick}>
         <spin className={classes.icon}>
             <CartIcon/>
         </spin>
