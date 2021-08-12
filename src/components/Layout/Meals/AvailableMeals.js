0df3024e55f7f2edf8f54b5f4d5302fdbdad8ba4
joshyjose <joshyjose12@gmail.com>
2021-08-12 11:48:14 +0200

    Implement Http and Loader

--- src/components/Layout/Meals/AvailableMeals.js
+++ src/components/Layout/Meals/AvailableMeals.js
@@ -1,37 +1,45 @@
+import { useEffect, useState } from 'react';
 import clasess from './AvailableMeals.module.css';
 import Card from '../Meals/UI/Card';
 import MealItem from './MealItem/MealItem';
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
 
   const AvailableMeals = () => {
 
-    const mealList = DUMMY_MEALS.map((meal) => <MealItem 
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+
+    const fetchMeals = async () => {
+    const response = await fetch('https://react-http-hooks-88948-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+    const resposeData = await response.json();
+   
+    const loadedMeals = [];
+
+    for(const key in resposeData) {
+      loadedMeals.push({
+        id:key,
+        name:resposeData[key].name,
+        description:resposeData[key].description,
+        price:resposeData[key].price
+
+      })
+    }
+    setMeals(loadedMeals);
+    setIsLoading(false);
+    };
+
+    fetchMeals();
+  
+  },[]);
+
+  if(isLoading)
+  {
+    return(<section className={clasess.MealsLoading}>Loading....</section>)
+  }
+
+    const mealList = meals.map((meal) => <MealItem 
       key={meal.id}
       id={meal.id}
       name={meal.name}
